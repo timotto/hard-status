29436b33fc355ce84af1f42bccde631958853142
Tim Otto <tim@timotto.io>
2018-02-02 00:13:08 +0100

    fixes

--- src/arduino/ci/prepare-arduino-esp32-docker.sh
+++ src/arduino/ci/prepare-arduino-esp32-docker.sh
@@ -8,4 +8,4 @@ cp -r arduino-esp32-source build/
 # This 'pointless' grep makes sure the expected line exists in the Dockerfile
 # If it didn't exist, the sed line would do nothing causing the result to be unexpected
 grep '^FROM .* AS base$' build/Dockerfile
-sed -es'/^FROM .* AS base$/FROM '"$FROM"' AS base/' -i build/Dockerfile
+sed -es'+^FROM .* AS base$+FROM '"$FROM"' AS base+' -i build/Dockerfile
