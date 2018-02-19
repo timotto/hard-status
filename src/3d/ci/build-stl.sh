542dd5f8bc2e083bb70c5e7e884077e9e11fe34f
Tim Otto <tim@timotto.io>
2018-02-19 19:20:30 +0100

    add missing task input and silence uninteresting error during stl build

--- src/3d/ci/build-stl.sh
+++ src/3d/ci/build-stl.sh
@@ -2,6 +2,11 @@
 
 set -e -x
 
+# workaround to silence this error message
+# ERROR: boost::filesystem::canonical: No such file or directory: "/root/.local/share"
+
+mkdir -p $HOME/.local/share
+
 openscad \
     -o stl-output/${NAME}.stl \
     --render \
