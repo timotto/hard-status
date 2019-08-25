d484bedcd29c42774c13c8be2edb3becde86537e
Tim Otto <tim@timotto.io>
2019-08-25 08:45:31 +0200

    feat: proper annotations, etc customization for ingress

--- src/main/ci/create-chart.sh
+++ src/main/ci/create-chart.sh
@@ -3,6 +3,7 @@
 set -e
 
 echo "app version: $(cat version/number)"
+echo "$VALUES_YAML" > source-code/src/main/helm/hard-status/values.yaml
 echo "version: $(cat version/number)" >> source-code/src/main/helm/hard-status/values.yaml
 
 helm init --client-only
